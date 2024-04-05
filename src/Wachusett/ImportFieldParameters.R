2167cc9cb6539f23e98a9300d6ee6b5ed7ce5e9c
Drury <travis.drury@mass.gov>
2024-04-05 14:38:41 -0400

    Adds check for matching staff gauge height or turbidity times in database. Stops if not found and shows error.

--- src/Wachusett/ImportFieldParameters.R
+++ src/Wachusett/ImportFieldParameters.R
@@ -220,6 +220,29 @@ if (length(dupes2) > 0){
 }
 rm(Uniq)
 
+### Make sure records have matching stage or turbidity times in database
+DateTimesMatch <- dbGetQuery(con, glue("SELECT DateTimeET, Location FROM {database}.{schema}.{ImportTable} WHERE Parameter IN ('Turbidity NTU','Staff Gauge Height')"))
+DateTimesMatch$DateTimeET <- DateTimesMatch$DateTimeET  %>% force_tz("America/New_York")
+
+### Keep only locations that always have matching stage or turbidity times
+locations_timecheck <- locations %>% filter(LocationType == "Tributary",
+                                            LocationCategory %in% c("Primary","Secondary","Long-term Forestry")) %>%
+  select(LocationMWRA)
+
+### Filter imported data to only the locations above
+df_no_MISC <- df %>% filter(Location %in% locations_timecheck$LocationMWRA)
+
+### Anti-join to only keep records without location/datetime matches in the database
+UnmatchedDateTimes <- anti_join(df_no_MISC, DateTimesMatch, by=c("DateTimeET"))
+
+### If any unmatched times exist, stop processing and show error to user
+if (nrow(UnmatchedDateTimes) > 0){
+  # Exit function and send a warning to user
+  stop(paste0("This data file contains ", nrow(UnmatchedDateTimes),
+              " records with Date/Times not present in Survey123 data for site(s): ", paste(unique(sort(UnmatchedDateTimes$Location)), collapse=", ")))
+  print(UnmatchedDateTimes) # Show the duplicate Unique IDs to user in Shiny
+}
+
 ### DataSource ####
 df$DataSource <- file
 
